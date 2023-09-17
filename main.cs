0311b31bc042c0bb578d7ef11a922d92be1b2ed9
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-09-17 04:58:08 +0000

    Main upload

--- main.cs
+++ main.cs
@@ -1,7 +1,21 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    private const int MAX_CHARACTERS = 140;
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your message:");
+        string message = Console.ReadLine();
+
+        if (message.Length > MAX_CHARACTERS)
+        {
+            Console.WriteLine("Rejected");
+        }
+        else
+        {
+            Console.WriteLine("Posted");
+        }
+    }
 }
